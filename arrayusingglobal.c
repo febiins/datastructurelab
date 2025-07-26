84992be3f160afe78636a85c2ae631709198dd76
febin <febinsunny2004@gmail.com>
2025-07-26 10:21:32 +0530

    program 6 and comments had been finished

--- arrayusingglobal.c
+++ arrayusingglobal.c
@@ -1,29 +1,30 @@
+/*The program is for demonstrating array operations using global variables in a menu driven format. Author: @Febin Sunny date:26-07-2025*/ 
 #include<stdio.h>
 
-int a[5],pos=-1;
+int a[5],pos=-1;  //global variables
 
-void insert(int e){
-	if(pos+1==5){
+void insert(int e){  //function for inserting
+	if(pos+1==5){   //checking the array is empty
 		printf("\n The array is full");
 	}
-	else{
+	else{   //if not element is inserted
 		a[++pos]=e;
 		printf("\n Element is inserted");
 	}
 }
 
-void delete(){
-	if(pos==-1){
+void delete(){    //function for deleting
+	if(pos==-1){  //checking the array is empty 
 		printf("\nThe array is empty");
 	}
-	else{
+	else{     //if not empty element in the current position is deleted
 		int del=a[pos];
 		printf("Element deleted is %d",del);
 		a[pos--];
 	}
 }
 
-void display(){
+void display(){  //function for displaying array elements
 	int i;
 	for(i=0;i<=pos;i++){
 		printf("%d\t",a[i]);
@@ -31,29 +32,29 @@ void display(){
 }
 
 
-int menu(){
+int menu(){  //menu function for choice
 	int ch;
 	printf("\nInsert-1 \n Delete-2 \n Display-3\n Quit-4\n ");
 	scanf("%d",&ch);
 	return ch;
 	
 }
-void process(){
+void process(){  //function which calls operation based on your choice
 	int choice,element;
 	for(choice=menu();choice!=4;choice=menu()){
 		switch(choice){
-			case 1:
+			case 1:         //insert
 				printf("\nEnter the element to insert");
 				scanf("%d",&element);
 				insert(element);
 				break;
-		    case 2:
+		    case 2:         //delete
 		    	delete();
 				break;	
-		    case 3:
+		    case 3:         //display
 		    	display();
 				break;	
-			case 4:
+			case 4:        //exit
 		    	
 				break;
 			
@@ -65,7 +66,7 @@ void process(){
 	}
 }
 int main(){
-	process();
+	process();  //calling the process function for the operation
 	
 	return 0;
 	
--- arrayusinglocal.c
+++ arrayusinglocal.c
@@ -1,13 +1,13 @@
+/*The program is for demonstrating array operations using local variables in a menu driven format. Author: @Febin Sunny date:26-07-2025*/ 
+
 #include<stdio.h>
 
+void insert(int a[5],int *pos,int e){  //function for inserting elements
 
-
-void insert(int a[5],int *pos,int e){
-
-	if(*pos+1==5){
+	if(*pos+1==5){  //checking the array is full
 		printf("\n The array is full");
 	}
-	else{
+	else{  //if not element is inserted
 		a[++*pos]=e;
 		printf("\n Element is inserted");
 	}
@@ -15,47 +15,47 @@ void insert(int a[5],int *pos,int e){
 
 }
 
-void delete(int a[5],int *pos){
-	if(*pos==-1){
+void delete(int a[5],int *pos){  //function for deleting elements
+	if(*pos==-1){  //checking the array is empty
 		printf("\nThe array is empty");
 	}
-	else{
+	else{  //if not empty element in the current position is deleted
 		int del=a[*pos];
 		printf("Element deleted is %d",del);
 		a[(*pos)--];
 	}
 }
 
-void display(int a[5],int *pos){
-	int i;
+void display(int a[5],int *pos){  //function for displaying elements
+	int i; //declaring variable
 	for(i=0;i<=*pos;i++){
 		printf("%d\t",a[i]);
 	}
 }
 
-int menu(){
-	int ch;
+int menu(){  //menu function for choice
+	int ch;  //delcartion variable  
 	printf("\nInsert-1 \n Delete-2 \n Display-3 \n Quit -4\n  ");
 	scanf("%d",&ch);
-	return ch;
+	return ch;  //returning the choice
 	
 }
-void process(int a[5],int *pos){
+void process(int a[5],int *pos){  //function which calls operation based on your choice
 	int choice,element;
 	for(choice=menu();choice!=4;choice=menu()){
 		switch(choice){
-			case 1:
+			case 1:            //insert
 				printf("\nEnter the element to insert");
 				scanf("%d",&element);
 				insert(a,pos,element);
 				break;
-		    case 2:
+		    case 2:           //delete
 		    	delete(a,pos);
 				break;	
-		    case 3:
+		    case 3:           //display
 		    	display(a,pos);
 				break;	
-			case 4:
+			case 4:          //exit
 		    	
 				break;
 			
@@ -67,8 +67,8 @@ void process(int a[5],int *pos){
 	}
 }
 int main(){
-	int a[5],pos=-1;
-	process(a,&pos);
+	int a[5],pos=-1;  //local variable
+	process(a,&pos); //passing the array and address of pos due to using pointer
 	
 	return 0;
 	
