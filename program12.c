8983f1d0a65dff93098bb44a276f3c76d35bead4
febin <febinsunny2004@gmail.com>
2025-07-27 21:23:15 +0530

    commented

--- program12.c
+++ program12.c
@@ -5,37 +5,43 @@ Date: 27-07-2025
 */
 #include<stdio.h>
 
+// Stack array and top pointer
 int stack[10],top = -1;
 
+// Function to push an element onto the stack
 void push(int e){
+    // Check for stack overflow
     if(top+1 == 10){
         printf("Stack is overflow\n");
     }else{
-        stack[++top]=e;
+        stack[++top]=e; // Increment top and add element
         printf("Element is pushed to stack\n");
     }
 }
 
+// Function to pop an element from the stack
 void pop(){
+    // Check for stack underflow
     if(top == -1){
         printf("Stack is underflow\n");
     }
     else{
-        stack[top--];
+        stack[top--]; // Decrement top to remove element
         printf("Element is poped from stack\n");
-
     }
 }
 
+// Function to view the top element of the stack
 void peek(){
+    // Check for stack underflow
     if(top == -1){
          printf("Stack is underflow\n");
-
     }else{
         printf("%d is the element\n",stack[top]);
     }
 }
 
+// Function to display menu and get user choice
 int choice(){
     int ch;
     printf("push-1\n pop-2 \n peek-3 \n quit-4\n");
@@ -43,20 +49,22 @@ int choice(){
     return ch;
 }
 
+// Function to handle menu-driven stack operations
 void menu(){
     int c,element;
+    // Loop until user chooses to quit (option 4)
     for(c = choice();c != 4; c = choice()){
         switch(c){
             case 1:
             printf("Enter element\n");
             scanf("%d",&element);
-            push(element);
+            push(element); // Push element onto stack
             break;
             case 2:
-            pop();
+            pop(); // Pop element from stack
             break;
             case 3:
-            peek();
+            peek(); // View top element
             break;
             case 4:
             break;
@@ -64,6 +72,7 @@ void menu(){
     }
 }
 
+// Main function to start the program
 int main(){
     menu();
     return 0;
