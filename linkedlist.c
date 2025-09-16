846d3b47650a884e4616adcba5bc4a9a30dccfcc
febin <febinsunny2004@gmail.com>
2025-09-16 09:52:40 +0530

    upto search and replace is done

--- linkedlist.c
+++ linkedlist.c
@@ -78,6 +78,30 @@ void sort(){
     }
 
 }
+
+void search(){
+    int a,b;
+    printf("Enter the element to search\n");
+    scanf("%d",&a);
+    printf("Enter element to replace\n");
+    scanf("%d",&b);
+    node *t,*prev;
+    t=head;
+    prev=head;
+    if(head==NULL){
+        printf("List is empty\n");
+
+    }else{
+        while(prev->next!=NULL && t->next->data!=a){
+            if(t->data==a){
+                printf("%d Element is found\n",t->data);
+            }
+            
+        }
+        prev->next->data=b;
+    }
+
+}
 void display(){
     node *t;
     t=head;
@@ -91,6 +115,54 @@ void display(){
     }
     printf("\n");
 }
+/*-----------------------------------------------------------------------------------------------------------*/
+/*Creating a new node for polynomial representation*/
+
+typedef struct poly{
+    int coeff;
+    int pow;
+    struct poly *next;
+} poly;
+poly *start=NULL;
+
+void insertpoly(){
+    int c,p;
+    printf("Enter coefficient and power\n");
+    scanf("%d%d",&c,&p);
+    poly *newnode;
+    newnode=(poly*)malloc(sizeof(poly));
+    newnode->coeff=c;
+    newnode->pow=p;
+    newnode->next=NULL;
+    if(start==NULL){
+        start=newnode;
+    }
+    else{
+        poly *t;
+        t=start;
+        while(t->next!=NULL){
+            t=t->next;
+}
+        t->next=(poly*)malloc(sizeof(poly));
+        t->next->coeff=c;
+        t->next->pow=p;
+        t->next->next=NULL;
+    }
+    
+
+}
+
+void poly_display(){
+    poly *t;
+    t=start;
+    while(t!=NULL){
+        printf("%dx^%d",t->coeff,t->pow);
+        t=t->next;
+
+    }
+
+}
+
 
 
 int main(){
@@ -101,6 +173,8 @@ int main(){
     deletion();
     display();
     sort(); 
+    display();
+    search();
     display();
     return 0;
 }
