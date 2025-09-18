3e2d61a5a988af2b9b9b35e8375d3f13fe866f20
febin <febinsunny2004@gmail.com>
2025-09-18 11:39:11 +0530

    program updated

--- program2.c
+++ program2.c
@@ -1,77 +1,120 @@
 /*
-The program is for demonstrating array operations using global variables in a menu driven format.
- Author: @Febin Sunny 
- Date:26-07-2025
- */ 
-#include<stdio.h>
+The program is for demonstrating array operations using global variables 
+in a menu driven format.
+Author: @Febin Sunny 
+Date:26-07-2025
+*/
+#include <stdio.h>
 
-int a[5],pos=-1;  //global variables
+int a[5], pos = -1; // global variables
 
-void insert(int e){  //function for inserting
-	if(pos+1==5){   //checking the array is empty
-		printf("\n The array is full");
-	}
-	else{   //if not element is inserted
-		a[++pos]=e;
-		printf("\n Element is inserted");
-	}
+void insert(int e) { // function for inserting
+    if (pos + 1 == 5) { // checking the array is full
+        printf("\nThe array is full");
+    } else { // if not full, element is inserted
+        a[++pos] = e;
+        printf("\nElement %d is inserted", e);
+    }
 }
 
-void delete(){    //function for deleting
-	if(pos==-1){  //checking the array is empty 
-		printf("\nThe array is empty");
-	}
-	else{     //if not empty element in the current position is deleted
-		int del=a[pos];
-		printf("Element deleted is %d",del);
-		a[pos--];
-	}
+void delete () { // function for deleting
+    if (pos == -1) { // checking the array is empty
+        printf("\nThe array is empty");
+    } else { // if not empty, element in the current position is deleted
+        int del = a[pos];
+        printf("\nElement deleted is %d", del);
+        pos--;
+    }
 }
 
-void display(){  //function for displaying array elements
-	int i;
-	for(i=0;i<=pos;i++){
-		printf("%d\t",a[i]);
-	}
+void display() { // function for displaying array elements
+    if (pos == -1) {
+        printf("\nArray is empty");
+        return;
+    }
+    printf("\nArray elements: ");
+    for (int i = 0; i <= pos; i++) {
+        printf("%d\t", a[i]);
+    }
 }
 
+void search(int key) { // function for searching an element
+    if (pos == -1) {
+        printf("\nArray is empty");
+        return;
+    }
+    for (int i = 0; i <= pos; i++) {
+        if (a[i] == key) {
+            printf("\nElement %d found at position %d", key, i);
+            return;
+        }
+    }
+    printf("\nElement %d not found", key);
+}
 
-int menu(){  //menu function for choice
-	int ch;
-	printf("\nInsert-1 \n Delete-2 \n Display-3\n Quit-4\n ");
-	scanf("%d",&ch);
-	return ch;
-	
+void sort() { // function for sorting elements
+    if (pos == -1) {
+        printf("\nArray is empty");
+        return;
+    }
+    for (int i = 0; i <= pos; i++) {
+        for (int j = i + 1; j <= pos; j++) {
+            if (a[i] > a[j]) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    printf("\nArray sorted successfully");
 }
-void process(){  //function which calls operation based on your choice
-	int choice,element;
-	for(choice=menu();choice!=4;choice=menu()){
-		switch(choice){
-			case 1:         //insert
-				printf("\nEnter the element to insert");
-				scanf("%d",&element);
-				insert(element);
-				break;
-		    case 2:         //delete
-		    	delete();
-				break;	
-		    case 3:         //display
-		    	display();
-				break;	
-			case 4:        //exit
-		    	
-				break;
-			
-				
-			default:
-			printf("Invlaid choice");						
-				
-		}
-	}
+
+int menu() { // menu function for choice
+    int ch;
+    printf("\n\n--- MENU ---");
+    printf("\n1. Insert");
+    printf("\n2. Delete");
+    printf("\n3. Display");
+    printf("\n4. Search");
+    printf("\n5. Sort");
+    printf("\n6. Quit");
+    printf("\nEnter your choice: ");
+    scanf("%d", &ch);
+    return ch;
 }
-int main(){
-	process();  //calling the process function for the operation
-	
-	return 0;
-	
+
+void process() { // function which calls operation based on your choice
+    int choice, element;
+    for (choice = menu(); choice != 6; choice = menu()) {
+        switch (choice) {
+        case 1: // insert
+            printf("\nEnter the element to insert: ");
+            scanf("%d", &element);
+            insert(element);
+            break;
+        case 2: // delete
+            delete ();
+            break;
+        case 3: // display
+            display();
+            break;
+        case 4: // search
+            printf("\nEnter the element to search: ");
+            scanf("%d", &element);
+            search(element);
+            break;
+        case 5: // sort
+            sort();
+            break;
+        case 6: // exit
+            break;
+        default:
+            printf("\nInvalid choice");
+        }
+    }
+}
+
+int main() {
+    process(); // calling the process function for the operation
+    return 0;
 }
