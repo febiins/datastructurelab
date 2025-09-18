c7c73ae708d1f342bda9ce0e24f9f5d567c43429
febin <febinsunny2004@gmail.com>
2025-09-18 11:38:14 +0530

    program completed

--- program21.c
+++ program21.c
@@ -1,57 +1,60 @@
-/* The program used to demonstarates stack operations*/
+/* The program used to demonstarates stack operations
+    @Febin Sunny
+    Rollno: 27
+    27-07-2025
+*/
 
 #include<stdio.h>
 #include<stdlib.h>
 
-#define max 30
+#define max 30 // Maximum size of the stack
 
-char stack[max];
-int top = -1;
+char stack[max]; // Array to implement stack
+int top = -1;    // Stack top pointer
 
+// Function to push an element onto the stack
 void push(char a){
     if(top==max-1){
-        printf("\nStack Overflow");
+        printf("\nStack Overflow"); // Stack is full
     }
     else{
-        stack[++top]=a;
+        stack[++top]=a; // Increment top and add element
         printf("\n%c pushed to stack\n",a);
     }
 }
 
+// Function to pop an element from the stack
 char pop(){
     if(top==-1){
-        printf("\nstack is underflow");
-
+        printf("\nstack is underflow"); // Stack is empty
     }else{
-      return  stack[top--];
-       
+        return stack[top--]; // Return top element and decrement top
     }
 }
 
-
+// Function to read input, perform stack operations, and display output
 void display(){
-    char input[max];
-    char output[max];
+    char input[max];  // Array to store input string
+    char output[max]; // Array to store output
     int i=0, k=0;
     
     printf("\nEnter the elements to stack\n");
-    scanf("%s",input);
+    scanf("%s",input); // Read input string
     for(i=0;input[i]!='\0';i++){
-        if(input[i]=='*'){
+        if(input[i]=='*'){ // If '*' encountered, pop from stack
             output[k++]=pop();
         }
         else{
-            push(input[i]);
+            push(input[i]); // Otherwise, push character to stack
         }
-
     }
     for(i=0;i<k;i++){
-        printf("%c",output[i]);
+        printf("%c",output[i]); // Print output characters
     }
-    
 }
 
+// Main function
 int main(){
-    display();
+    display(); // Call display function to perform stack operations
     return 0;
 }
