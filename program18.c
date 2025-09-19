2ec3ff78b4fc6facaa356bfe454f60cceddafc08
febin <febinsunny2004@gmail.com>
2025-09-19 12:17:48 +0530

    compeleted

---
+++ program18.c
@@ -0,0 +1,139 @@
+/* program used to demonstrate the conversion of infix to prefix expression
+   @Febin Sunny
+   Rollno:27
+   18-09-2025
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+#define max 30       // Maximum size of the stack
+
+char stack[max];    // Array to implement stack
+int top = -1;      // Stack top pointer
+
+void push(char a){
+    if(top == max - 1){
+        printf("\nStack Overflow");   // Stack is full
+    }
+    else{
+        stack[++top] = a;     // Increment top and add element
+    }
+}
+
+char pop(){
+    if(top == -1){
+        printf("\nStack Underflow");    // Stack is empty
+        return '\0';
+    }
+    else{
+        return stack[top--];    // Return top element and decrement top
+    }
+}
+
+char peek(){
+    if(top == -1){
+        return '\0';     // Empty stack
+    }
+    else{
+        return stack[top];    // Return top element without decrementing top
+    }
+}
+
+int precedence(char a){   //function to check precedence
+    switch(a)
+    {
+        case '+':
+        case '-': return 1;
+        case '*':
+        case '/': return 2;
+        case '^': return 3;
+        default: return 0;
+    }
+}
+
+int isoperator(char a){
+    if(a == '+' || a == '-' || a == '*' || a == '/' || a == '^'){
+        return 1;   // Character is an operator
+    }
+    return 0;      // Not an operator
+}
+
+void reverse(char b[]){  //function for reversing the expression
+    int n=strlen(b);
+    int i;
+    for(i=0;i<n/2;i++){
+        char t;
+        t=b[i];
+        b[i]=b[n-i-1];
+        b[n-i-1]=t;
+
+    }
+    for(i=0;i<n;i++){
+        if(b[i]=='('){
+            b[i]=')';
+        }
+        else if(b[i]==')'){
+            b[i]='(';
+        }
+    }
+}
+
+void infixtopostfix(char infix[],char postfix[]){
+    
+    int i, j = 0;
+    char ch;
+
+    for(i = 0; infix[i] != '\0'; i++){
+        ch = infix[i];
+        
+        if(isalnum(ch)){   // operand
+            postfix[j++] = ch;
+        }
+        else if(ch == '('){
+            push(ch);
+        }
+        else if(ch == ')'){
+            while(top != -1 && peek() != '('){
+                postfix[j++] = pop();
+            }
+            pop(); // remove '('
+        }
+        else if(isoperator(ch)){
+            while(top != -1 && precedence(peek()) >= precedence(ch)){
+                if(ch == '^' && peek() == '^'){  // handle right associativity
+                    break;
+                }
+                postfix[j++] = pop();
+            }
+            push(ch);
+        }
+    }
+
+    // Pop remaining operators
+    while(top != -1){
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';  // Null terminate
+    printf("Postfix expression is: %s\n", postfix);
+}
+
+void infixtoprefix(){
+    char infix[max],postfix[max],prefix[max];
+     printf("\nEnter the infix expression:\n");
+     scanf("%s", infix);
+    reverse(infix);
+    infixtopostfix(infix,postfix);
+    strcpy(prefix, postfix);
+    reverse(prefix);
+    printf("Prefix expression is: %s\n", prefix);
+}
+
+int main(){
+    infixtoprefix();
+    return 0;
+}
+
